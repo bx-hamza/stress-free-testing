73089df0a6f481a6b6bf6570340ac33d49d86462
Hamza <hamza@biobox.io>
2022-11-01 15:35:11 -0400

    wip

--- src/components/Tabs.cy.jsx
+++ src/components/Tabs.cy.jsx
@@ -16,10 +16,14 @@ const items = [
   },
 ]
 
+let onSelectSpy;
+
 describe('<Tabs />', () => {
-  it.only('renders the items using object syntax', () => {
+  beforeEach(() => {
+    onSelectSpy = cy.spy().as('selectSpy')
+  })
+  it('renders the items using object syntax', () => {
     cy.viewport(400, 200)
-    const onSelectSpy = cy.spy().as('selectSpy')
     cy.mount(Tabs, {
       props: {
         onSelect: onSelectSpy,
@@ -30,7 +34,6 @@ describe('<Tabs />', () => {
 
   it('renders the items', () => {
     cy.viewport(400, 200)
-    const onSelectSpy = cy.spy().as('selectSpy')
     cy.mount(() => (
       <div class="m-8">
         <Tabs onSelect={onSelectSpy} items={items} />
@@ -44,5 +47,22 @@ describe('<Tabs />', () => {
 
     cy.findByText(items[1].text).click().should('have.focus')
     cy.get('@selectSpy').should('have.been.calledWith', items[1])
+  })
+
+  it('renders all the items passed in', () => {
+    cy.mount(() => (
+      <Tabs items={items} />
+    )).wrap(items)
+    .each((item) => {
+      cy.get('body').findByText(item.text).should('be.visible').and('contain.text',item.text)
+    })
+
+    // Things to assert:
+    // 1. The event was emitted
+    // 2. The focus state is set (See `have.focus` assertion)
+    // 3. Keybindings are working as expected
+
+    // cy.findByText(items[1].text).click().should('have.focus')
+    // cy.get('@selectSpy').should('have.been.calledWith', items[1])
   })
 })
