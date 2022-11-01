d04f95d9b62f832d8e702c0e10235eb492872d0f
Hamza <hamza@biobox.io>
2022-11-01 15:47:01 -0400

    Pass in slot to determine what's going on

--- src/components/Card.cy.jsx
+++ src/components/Card.cy.jsx
@@ -2,14 +2,16 @@ import Card from './Card.vue'
 import CoffeeIcon from '~icons/mdi/coffee'
 import { faker } from '@faker-js/faker'
 
+const defaultContent = faker.lorem.words(10)
+
 describe('<Card/>', () => {
   it('renders the default slot', () => {
     cy.mount(
       <Card image={false} class="m-10 text-center group">
         {/* Consider using data-testid or cy.findByText to target and test if the default slot was rendered */}
         <CoffeeIcon class="text-4xl mx-auto group-hover:text-blue-600 duration-250 transition" />
-        <div>{faker.lorem.words(10)}</div>
+        <div data-testid="default-slot">{defaultContent}</div>
       </Card>,
-    )
+    ).get('[data-testid=default-slot]')
   })
 })
